c1bdc7fe75de2650edd13933b56c23c17e43ae03
Corey Bergeron <coreybergeron1@gmail.com>
2020-02-27 14:31:02 -0500

    show/hide working

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 window.$ = window.jQuery = require('jquery');
 
-$("input[type = radio]:checked").change(function () {
+// console.log( $("input[type = radio]:checked").val() )
+$('#spouse').hide()
+
+$("input[type = radio]").change(function () {
     var radioValue = $("input[type = 'radio']:checked").val();
-    if(radioValue == "yes") {
-        $('div.spouse').show();
+    // console.log(typeof(radioValue))
+    if (radioValue === "yes") {
+        // console.log("YES")
+        $('#spouse').show();
     } else {
-        $('div.spouse').hide();
+        // console.log("NO")
+        $('#spouse').hide();
     }
 })
 
